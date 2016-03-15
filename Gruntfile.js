661c28020f9a0aa70662f4b6b6e74e4a9295225c
Matias De Carli <matiasdecarli@gmail.com>
2016-03-15 15:50:41 -0300

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+/*
+ * grunt-conditional-deploy
+ * https://github.com/matiasdecarli/grunt-conditional-deploy
+ *
+ * Copyright (c) 2016 Matias De Carli
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Configuration to be run (and then tested).
+    'conditional_deploy': {
+      options:{
+        separator: '\n'
+      },
+      frontend:{
+        options:{
+          target: 'frontend'
+        },
+        files: [{
+          src: [
+            'src/**'
+          ],
+          dest: '/',
+          expand: true,
+          filter: 'isFile'
+        }]
+      },
+      backend:{
+        options:{
+          target: 'backend'
+        },
+        files: [{
+          src: [
+            'lib/**'
+          ],
+          dest: '/',
+          expand: true,
+          filter: 'isFile'
+        }]
+      }
+    },
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['conditional_deploy']);
+
+};
---
+++ tasks/conditional_deploy.js
@@ -0,0 +1,45 @@
+/*
+ * grunt-conditional-deploy
+ * https://github.com/matiasdecarli/grunt-conditional-deploy
+ *
+ * Copyright (c) 2016 Matias De Carli
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var async = ('async');
+
+module.exports = function(grunt) {
+
+	var spawn = require("child_process").spawn,
+	        donePromise;
+
+	var _ = require('lodash');
+
+    var outputData = function (obj) {
+        var i = 0;
+    var commit = grunt.util.spawn({
+            cmd: "git",
+			args: ['diff', '--name-only', 'HEAD~1'],
+        }, function(error, result, code) {
+        	if (error) grunt.fail.fatal(error);
+
+        	var files = result.stdout.split(obj.options().separator);
+
+    		if (_.intersection(obj.filesSrc,files).length!==0) runTask(obj.data.options.target);
+
+            donePromise();
+        });
+    };
+
+    function runTask(taskName){
+    	grunt.task.run(taskName);
+    	console.log('should run: ', taskName);
+    }
+
+  grunt.registerMultiTask('conditional_deploy', 'A plugin to conditionally run tasks based on files on the last commit', function() {
+	 	donePromise = this.async();
+	 	outputData(this);
+  });
+};
